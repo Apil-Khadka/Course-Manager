920d8bedc6a1a4e2097f8fe4b3361af3d962c301
Night Silent <apilkhadka2020@gmail.com>
2024-09-06 13:14:07 +0545

    Inital Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Courses-Lessons
+A laravel based react app , to create courses and assign lessons .
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'God Win',
+            'email' => 'godwin@example.com',
+            'password' => bcrypt('YouGotGuts'),
+            'admin' => true,
+        ]);
+        User::factory(22)->create();
+
+        $this->call(CourseSeeder::class);
+        $this->call(LessonSeeder::class);
+    }
+}
